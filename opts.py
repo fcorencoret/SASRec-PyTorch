65c00888d93171510022e5c3d725cec3437d1361
fcorencoret <frencoret@uc.cl>
2018-11-16 11:19:35 -0300

    opts fix

--- opts.py
+++ opts.py
@@ -2,8 +2,8 @@ import argparse
 parser = argparse.ArgumentParser(description="PyTorch SASRec")
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--n_epochs', default=10, type=int,
+                    help='number of epochs')
 
 
 
